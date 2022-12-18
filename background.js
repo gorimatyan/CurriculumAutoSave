ea1dec4342963ffd0e80d45535e76ab6be271755
gorimatyan <gorima102131@gmail.com>
2022-12-18 15:00:05 +0900

    イベントリスナーを2つ追加

--- background.js
+++ background.js
@@ -1,12 +1,30 @@
-chrome.tabs.onActivated.addListener((activeInfo) =>
-    chrome.scripting.executeScript({
-        target: { tabId: activeInfo.tabId },
-        files: ["autoSave.js"]
-    })
-  )
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>
+/**
+ * 現在のタブを取得
+ * @returns 現在のタブ
+ */
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+// タブが閉じられたときに発火
+chrome.tabs.onRemoved.addListener((tab) => {
   chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      files: ["autoSave.js"]
-  })
-)
+    target: { tabId: tab.id },
+    files: ["scripts/idleAutoSave.js"],
+  });
+});
+
+// カリキュラム作成ページを15秒以上放置した場合に発火
+chrome.idle.setDetectionInterval(15);
+chrome.idle.onStateChanged.addListener((newState) => {
+  if (newState == "idle") {
+    getCurrentTab().then((tab) =>
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ["scripts/idleAutoSave.js"],
+      })
+    );
+  }
+});
